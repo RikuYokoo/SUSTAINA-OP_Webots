90c019dcc20fdd2a84ae19a90dcfb97da65c6a4c
RikuYokoo <s19C1123JF@s.chibakoudai.jp>
2025-02-14 09:29:21 +0900

    Add Docker environment

---
+++ launch_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker_image=""
+
+id=${1:-"1"}
+
+xhost +
+docker run --gpus=all -it --rm -v /tmp/.X11-unix:/tmp/.X11-unix:rw --privileged \
+		   -e DISPLAY \
+                   -v $(pwd):/home/user/webots_ws/ \
+		   --name webots_$id \
+		   --env="QT_X11_NO_MITSHM=1" \
+		   $docker_image \
+                   webots --batch webots/world/collect_image.wbt
